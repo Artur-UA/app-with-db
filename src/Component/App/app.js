86f0b67c2e18a9da206cc10be5c22101f87ec951
Artur <kovalskartur@yahoo.com>
2022-02-22 23:44:09 +0200

    second commit

--- src/Component/App/app.js
+++ src/Component/App/app.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 
+import Header from "../header/header";
+import ItemList from "../item-list/item-list";
+import PersonDetails from "../person-details/person-details";
+import RandomPlanet from "../random-planet/random-planet";
 export default class App extends Component{
     render (){
-        return <div>Hello World</div>
+        return (
+        <div>
+            <Header />
+            <RandomPlanet />
+      
+            <div className="row mb2">
+              <div className="col-md-6">
+                <ItemList />
+              </div>
+              <div className="col-md-6">
+                <PersonDetails />
+              </div>
+            </div>
+          </div>
+        );
     }
 }
